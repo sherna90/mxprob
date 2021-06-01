c4984620763959015d386e1dbad5244146cebece
Sergio Hernandez <sherna90@yahoo.es>
2021-06-01 11:12:55 -0400

    hmc

--- hamiltonian/inference/hmc.py
+++ hamiltonian/inference/hmc.py
@@ -6,35 +6,44 @@ import mxnet.gluon.probability as mxp
 from tqdm import tqdm, trange
 from copy import deepcopy
 from hamiltonian.inference.base import base
+from hamiltonian.inference.find_reasonable_epsilon import DualAveragingStepSize
 
 class hmc(base):
 
     def fit(self,epochs=100,burn_in=30,path_length=1.0,verbose=False,rng=None,**args):
         if rng == None:
-            rng = mx.random.seed(seed_state=1,ctx=self.ctx)
+            rng = mx.random.seed(seed_state=np.random.random_integers(0,100),ctx=self.ctx)
         q,p=self.start,self.draw_momentum(rng)
         loss=np.zeros(int(epochs))
+        rate=list()
+        epsilons=list()
         samples={var:[] for var in self.start.keys()}
-        for i in tqdm(range(int(epochs+burn_in))):
+        step_size_tuning = DualAveragingStepSize(initial_step_size=self.step_size)
+        for idx in tqdm(range(int(epochs+burn_in))):
             q_new,p_new=self.step(q,p,path_length,rng,**args)
             acceptprob = self.accept(q, q_new, p, p_new,**args)
             if acceptprob and (np.random.rand() < acceptprob): 
                 q = q_new.copy()
                 p = p_new.copy()
-            if verbose and (i%(epochs/10)==0):
-                print('loss: {0:.4f}'.format(loss[i]))
-            if i>burn_in:
+            # Tuning routine
+            if idx < burn_in - 1:
+                self.step_size, _ = step_size_tuning.tune(acceptprob)
+            elif idx == burn_in - 1:
+                _, self.step_size = step_size_tuning.tune(acceptprob)   
+            if idx > burn_in:
+                rate.append(acceptprob)
                 for var in self.start.keys():
                     samples[var].append(q[var])
-        return samples,loss
+        return samples,rate
 
     def step(self,state,momentum,path_length, rng,**args):
         q = state.copy()
         p = momentum.copy()
         q_new = deepcopy(q)
-        p_new = deepcopy(p)
-        epsilon=self.step_size
-        path_length=np.ceil(2*np.random.rand()*path_length/epsilon)
+        p_new = self.draw_momentum(rng)
+        epsilon=float(self.step_size)
+        clip  = lambda val :  max(min(100,val),2)
+        L=clip(np.ceil(path_length/epsilon))
         for var in q_new.keys():
             q_new[var].attach_grad()
         with autograd.record():
@@ -43,12 +52,13 @@ class hmc(base):
         for var in q_new.keys():
             p_new[var]-= (0.5*epsilon)*q_new[var].grad     
         # leapfrog step 
-        for _ in np.arange(path_length-1):
+        for _ in range(int(L-1)):
             for var in self.start.keys():
                 q_new[var]+= epsilon*p_new[var]
-                with autograd.record():
-                    loss = self.model.loss(q_new,**args)
-                loss.backward()
+            with autograd.record():
+                loss = self.model.loss(q_new,**args)
+            loss.backward()
+            for var in self.start.keys():
                 p_new[var]-= (0.5*epsilon)*q_new[var].grad
         for var in self.start.keys():
             q_new[var]+= epsilon*p_new[var]
@@ -56,15 +66,14 @@ class hmc(base):
             loss = self.model.loss(q_new,**args)
         for var in self.start.keys():
             p_new[var]-= epsilon*q_new[var].grad
-        for var in self.start.keys():
             p_new[var]=-p_new[var]
         return q_new,p_new
 
 
     def accept(self,current_q, proposal_q, current_p, proposal_p,**args):
-        E_new = (self.model.loss(proposal_q,**args)+self.potential_energy(proposal_p))
+        E_proposal = (self.model.loss(proposal_q,**args)+self.potential_energy(proposal_p))
         E_current = (self.model.loss(current_q,**args)+self.potential_energy(current_p))
-        A = min(1.,np.exp(E_current-E_new))
+        A = min(1.,nd.exp(E_proposal-E_current).asnumpy())
         return A
 
 
@@ -85,16 +94,6 @@ class hmc(base):
             dtype=self.start[var].dtype) for var in self.start.keys()}
         return momentum
 
-            
-    def find_reasonable_epsilon(self,p_accept,**args):
-        self.t += 1
-        g=self.target_accept - p_accept
-        self.error_sum += self.target_accept - p_accept
-        #self.error_sum  = (1.0 - 1.0/(self.t + self.t0)) * self.error_sum + g/(self.t + self.t0)
-        log_step = self.mu - self.prox_center - (self.t ** 0.5) / self.gamma * self.error_sum
-        eta = self.t **(-self.kappa)
-        self.log_averaged_step = eta * log_step + (1 - eta) * self.log_averaged_step
-        return np.exp(log_step), np.exp(self.log_averaged_step)
 
         
 
