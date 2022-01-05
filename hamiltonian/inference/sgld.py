96766760d1bf6001e96adf9fd171c880007f591d
sherna90 <sherna90@yahoo.es>
2022-01-05 09:36:06 -0300

    Update sgld.py

--- hamiltonian/inference/sgld.py
+++ hamiltonian/inference/sgld.py
@@ -48,7 +48,7 @@ class sgld(base):
             loss_val[i]=cumulative_loss/n_examples
             if dataset:
                 for var in params.keys():
-                    dataset[var][chain,i,:]=params[var].asnumpy()
+                    dataset[var][chain,i,:]=params[var].data().asnumpy()
             if verbose and (i%(epochs/10)==0):
                 print('loss: {0:.4f}'.format(loss_val[i]))
         #posterior_samples_single_chain={var:np.asarray(single_chain[var]) for var in single_chain}
